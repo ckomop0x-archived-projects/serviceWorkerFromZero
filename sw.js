446e2d5e59d1efcb56bbde1d625140234ebd1373
Pavel Klochkov <paul.klochkov@gmail.com>
2019-05-02 21:23:28 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,19 +1,26 @@
 // console.log('service Worker self', self);
+self.skipWaiting();
 
-
+self.addEventListener("fetch", event => {
+  throw Error("error in fetch handler");
+  event.respondWith(fetch(event.request.url));
+})
 
 self.addEventListener("install", () => {
   console.log("new SW installed")
 })
 
-self.addEventListener("fetch", event => {
-  console.log('event.request.url', event.request.url)
-  // fetch(event.request.url).then(result => console.log('fetch(event.request.url', result))
-  event.respondWith(fetch(event.request.url))
-  // event.respondWith(new Response("hello!"))
-})
 
-// self.skipWaiting()
-self.addEventListener("activate", () => {
-  console.log("new SW activated immediately 2")
-})
+// self.addEventListener("fetch", event => {
+//   console.log('event.request.url', event.request.url)
+//   // fetch(event.request.url).then(result => console.log('fetch(event.request.url', result))
+//   event.respondWith(fetch(event.request.url))
+//   // event.respondWith(new Response("hello!"))
+// })
+//
+// // self.skipWaiting()
+// self.addEventListener("activate", () => {
+//   console.log("new SW activated immediately 2")
+// })
+
+
