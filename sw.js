60313e8d5bc1dd2ce5c4eedb02e5e1c337427a4e
Pavel Klochkov <paul.klochkov@gmail.com>
2019-05-02 20:39:51 +0200

    Updates

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
-console.log('service Worker self', self);
-
-self.addEventListener("fetch", event => {
-  // console.log(event)
-  console.log('event.request.url', event.request.url)
-  fetch(event.request.url).then(result => console.log('fetch(event.request.url', result))
-  event.respondWith(fetch(event.request.url))
-  // event.respondWith(new Response("hello!"))
-})
-
-// self.skipWaiting()
-self.addEventListener("activate", () => {
-  console.log("new SW activated immediately 2")
-})
+// console.log('service Worker self', self);
+//
+// self.addEventListener("fetch", event => {
+//   // console.log(event)
+//   console.log('event.request.url', event.request.url)
+//   fetch(event.request.url).then(result => console.log('fetch(event.request.url', result))
+//   event.respondWith(fetch(event.request.url))
+//   // event.respondWith(new Response("hello!"))
+// })
+//
+// // self.skipWaiting()
+// self.addEventListener("activate", () => {
+//   console.log("new SW activated immediately 2")
+// })
